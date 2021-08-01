5a6cd8b7fb7cd6e2b00b2c17d278b3a8db89766f
IsaacWl <Isaacsilva5@hotmail.com>
2021-07-31 22:41:50 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    margin: 10,
+    loop: true,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
